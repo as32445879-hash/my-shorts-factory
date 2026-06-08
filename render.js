f9bd77016c59e635eafcadb65547d671fd7d4858
as32445879-hash <as32445879@gmail.com>
2026-06-08 11:36:14 +0900

    Fix API key assignment syntax in render.js

--- render.js
+++ render.js
@@ -2,8 +2,7 @@ const Shotstack = require('shotstack-sdk');
 
 const defaultClient = Shotstack.ApiClient.instance;
 const DeveloperKey = defaultClient.authentications['DeveloperKey'];
-DeveloperKey.apiKey = "Ii71IYF3v62bXyUu96zH84RpMdaK5E1a7N9Ww2qC
-"; // 2단계에서 채울 곳!
+DeveloperKey.apiKey = "Ii71IYF3v62bXyUu96zH84RpMdaK5E1a7N9Ww2qC";
 const api = new Shotstack.EditApi();
 
 const videoAsset = new Shotstack.VideoAsset();
